dac0d43590062d6e227a0aa0c442446703b3e668
CHRoosipuu <carl@chroosipuu.com>
2020-09-10 12:51:39 -0400

    Sidebar update

--- scenes/sidebarScene.js
+++ scenes/sidebarScene.js
@@ -4,6 +4,15 @@ class SidebarScene extends Phaser.Scene {
     }
 
     preload() {
+        // Load star images
+        this.load.image("friends", "assets/images/other/sidebar/friends-icon.png");
+        this.load.image("wordbank", "assets/images/other/sidebar/wordbank-icon.png");
+
+        this.load.scenePlugin({
+            key: 'rexuiplugin',
+            url: 'https://raw.githubusercontent.com/rexrainbow/phaser3-rex-notes/master/dist/rexuiplugin.min.js',
+            sceneKey: 'rexUI'
+        });
     }
 
     create() {
@@ -13,7 +22,47 @@ class SidebarScene extends Phaser.Scene {
 
         var background = this.add.rectangle(0, 0, container.width, container.height, 0xC2EAE9);
         background.setOrigin(0, 0);
+
+
+        this.createButtons();
         container.add(background);
+        container.add(this.answerGridButtons);
+    }
+
+    createButtons() {
+        let width = 80;
+        let height = this.cameras.main.height / 3;
+
+        this.answerGridButtons = this.rexUI.add.gridButtons({
+            width: width, height: height,
+            anchor: {left: "left", top: "top"},
+
+            buttons: [
+                [this.createButton('wordbank')],
+                [this.createButton('friends')]
+            ],
+            space: {
+                left: 10, right: 10, top: 20, bottom: 20,
+                row: 20, column: 20
+            },
+
+        })
+            .layout()
+            .on('button.click', function (button, index, pointer, event) {
+                console.log(button);
+            })
+    }
+
+    createButton(text) {
+        let icon = this.add.image(0, 0, text);
+        icon.displayWidth = 80;
+        icon.scaleY = icon.scaleX;
+        return this.rexUI.add.label({
+            width: 40,
+            height: 10,
+            icon: icon,
+            align: 'center'
+        });
     }
 
 }
